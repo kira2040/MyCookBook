09239ea5b4111ebaebf4c7f9f64cef50de3ea901
rerison <kira2040@gmail.com>
2021-04-01 17:02:24 +0100

    variable python

---
+++ variable_types.py
@@ -0,0 +1,10 @@
+my_integer = 3
+my_float = 3.5145
+my_string= "Hello"
+
+print(type(my_integer))
+print(type(my_float))
+print(type(my_string))
+
+my_integer = "you can change the type"
+print("change the tipe"+str(my_integer))
