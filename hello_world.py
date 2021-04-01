1ac115f48e02612306c3e398e576c344bda43140
rerison <kira2040@gmail.com>
2021-04-01 17:10:42 +0100

    update

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,11 @@
+from datetime import datetime
 print("Hallo World")
-input("what is your name: ")
+
+name = input("what is your name: ")
+
+number=int(input("When did you born : "))
+
+age = datetime.today().year.__int__()
+
+
+print(name + "you are " + str(age-number)+" year old")
